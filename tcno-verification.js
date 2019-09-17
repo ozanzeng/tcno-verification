8e3a0a41bd0b3af3e84afc567c13c42f3ee9d924
Ozan Zengin <40130731+ozanzeng@users.noreply.github.com>
2019-09-17 10:30:45 +0300

    Update tcno-verification.js
    Update last 9 digit code

--- tcno-verification.js
+++ tcno-verification.js
@@ -5,6 +5,8 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = void 0;
 
+function _readOnlyError(name) { throw new Error("\"" + name + "\" is read-only"); }
+
 var TCNoVerification = function TCNoVerification(identyNumber) {
   var replacedText = identyNumber.replace(/\D+/g, '');
   if (replacedText.substring(0, 1) === '0' || replacedText.length !== 11) return false;
@@ -17,13 +19,12 @@ var TCNoVerification = function TCNoVerification(identyNumber) {
   var TCNoFirstNineArray = identyNumber.substring(0, 9).split('');
   var sumOfEvenIndexes = 0;
   var sumOfOddIndexes = 0;
-  for(var i=0;i<9;i++){
-    if(i%2==0)
-      sumOfEvenIndexes += parseInt(TCNoFirstNineArray[i]);
-    else
-      sumOfOddIndexes += parseInt(TCNoFirstNineArray[i]);
+
+  for (var i = 0; i < 9; _readOnlyError("i"), i++) {
+    if (i % 2 == 0) sumOfEvenIndexes += (_readOnlyError("sumOfEvenIndexes"), parseInt(TCNoFirstNineArray[i]));else sumOfOddIndexes += (_readOnlyError("sumOfOddIndexes"), parseInt(TCNoFirstNineArray[i]));
   }
-  var TCNoSecondFromTheLastDigit = ((sumOfEvenIndexes * 7 - sumOfOddIndexes) % 10 + 10) % 10; 
+
+  var TCNoSecondFromTheLastDigit = ((sumOfEvenIndexes * 7 - sumOfOddIndexes) % 10 + 10) % 10;
   return TCNoLastDigit === +identyNumber[10] & TCNoSecondFromTheLastDigit == +identyNumber[9];
 };
 
