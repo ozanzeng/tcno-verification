2e08c12ae8a3a1c5625bbdd6d43b0200f87f78da
Ozan Zengin <40130731+ozanzeng@users.noreply.github.com>
2019-09-17 10:30:18 +0300

    Update tcno-verification-dev.js
    Update last 9 digit code

--- tcno-verification-dev.js
+++ tcno-verification-dev.js
@@ -10,7 +10,19 @@ const TCNoVerification = identyNumber => {
       .map(i => parseInt(i))
       .reduce((a, b) => a + b) % 10;
 
-  return TCNoLastDigit === +identyNumber[10];
+  const TCNoFirstNineArray = identyNumber.substring(0, 9).split('');
+  const sumOfEvenIndexes = 0;
+  const sumOfOddIndexes = 0;
+
+  for(const i=0;i<9;i++){
+    if(i%2==0)
+      sumOfEvenIndexes += parseInt(TCNoFirstNineArray[i]);
+    else
+      sumOfOddIndexes += parseInt(TCNoFirstNineArray[i]);
+  }
+  
+  const TCNoSecondFromTheLastDigit = ((sumOfEvenIndexes * 7 - sumOfOddIndexes) % 10 + 10) % 10; 
+  return TCNoLastDigit === +identyNumber[10] & TCNoSecondFromTheLastDigit == +identyNumber[9];
 };
 
 export default TCNoVerification;
